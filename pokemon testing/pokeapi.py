cd43ec33b6333f7319fecdae0652e7981f80d391
Ariel Liu <44077856+arielycliu@users.noreply.github.com>
2023-09-19 11:28:53 -0400

    Added code for pokemon popup

--- pokemon testing/pokeapi.py
+++ pokemon testing/pokeapi.py
@@ -5,18 +5,7 @@ import urllib.request
 response = requests.get("https://pokeapi.co/api/v2/pokemon/ditto")
 sprite_list = response.json()["sprites"]
 url = sprite_list["front_default"]
-urllib.request.urlretrieve(
-    url,
-    "pokemon.png")
-img = Image.open("pokemon.png")
-
-from PIL import Image
-import requests
-from io import BytesIO
-
-# response = requests.get(url)
-# img = Image.open(BytesIO(response.content))
-
-x = Image.open('pokemon.png').quantize(colors=10,method=1)
+urllib.request.urlretrieve(url, "pokemon.png")
+x = Image.open('pokemon.png').quantize(colors=10, method=1)
 x = x.convert('RGB')
 x.show()
